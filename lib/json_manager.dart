2d5db72c464e7374a62a89868dc06526458cee80
murilinhops <murilo.pescatori@nubank.com.br>
2023-01-20 15:29:47 -0300

    Add method to load storyteller json

--- lib/json_manager.dart
+++ lib/json_manager.dart
@@ -2,22 +2,31 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart' show rootBundle;
 
-import 'models.dart';
+import 'models/models.dart';
 
-class AdventureJson {
+class JsonManager {
   final String jsonKey = 'gameHistory';
 
   static Future<String> _loadJson(String json) async {
     return await rootBundle.loadString(json);
   }
 
-  static Future<AllNarrativeNodesList> loadNarrative(
-      {required String file}) async {
+  static Future<AllNarrativeNodesList> loadNarrative({
+    required String file,
+  }) async {
     final jsonProduct = await _loadJson(file);
     final jsonResponse = json.decode(jsonProduct);
 
     final allNarrativeNodes = AllNarrativeNodesList.fromJson(jsonResponse);
 
     return allNarrativeNodes;
+  }
+
+  static Future<WithdrawLines> loadWithdrawLines({
+    required String file,
+  }) async {
+    final jsonResponse = json.decode(await _loadJson(file));
+
+    return WithdrawLines.fromJson(jsonResponse);
   }
 }
