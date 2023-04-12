4d3c1e7f3c29faa4e0a5adb8a29b361fb8b955a7
murilinhops <murilo.pescatori@nubank.com.br>
2023-04-12 10:44:45 -0300

    Update jsonManager logic

--- lib/data/json_data_manager.dart
+++ lib/data/json_data_manager.dart
@@ -6,6 +6,13 @@ import 'package:txt_game_poc/data/narrador_end_lines_data_model.dart';
 import 'package:txt_game_poc/utils/player_prefs_save.dart';
 
 class JsonDataManager {
+  JsonDataManager({
+    required this.narrativeJsonPath,
+    required this.narradorLinesJsonPath,
+  });
+  final String narrativeJsonPath;
+  final String narradorLinesJsonPath;
+
   late NarrativeNodesListDataModel narrativeNodesList;
   late NarradorEndLinesDataModel narradorEndLinesData;
 
@@ -13,24 +20,25 @@ class JsonDataManager {
     return await rootBundle.loadString(json);
   }
 
-  Future<NarrativeNodesListDataModel> loadNarrative({
-    required String filePath,
-  }) async {
+  Future<NarrativeNodesListDataModel> loadNarrative() async {
     final savedNarrative = await PlayerPrefs.getHistoryFromPrefs();
     if (savedNarrative != null) return savedNarrative;
 
-    final jsonProduct = await _loadJson(filePath);
+    final jsonProduct = await _loadJson(narrativeJsonPath);
     final jsonResponse = json.decode(jsonProduct);
     return narrativeNodesList =
         NarrativeNodesListDataModel.fromJson(jsonResponse);
   }
 
-  Future<NarradorEndLinesDataModel> loadWithdrawLines({
-    required String filePath,
-  }) async {
-    final jsonResponse = json.decode(await _loadJson(filePath));
+  Future<NarradorEndLinesDataModel> loadNarradorEndLines() async {
+    final jsonResponse = json.decode(await _loadJson(narradorLinesJsonPath));
 
     return narradorEndLinesData =
         NarradorEndLinesDataModel.fromJson(jsonResponse);
   }
+
+  Future<List> initData() async => [
+        await loadNarradorEndLines(),
+        await loadNarrative(),
+      ];
 }
